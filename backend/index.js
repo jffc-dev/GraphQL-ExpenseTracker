bee3717a86dbe275cd8bdd1b315aa0adba4bf13e
Javier Flores <jffc.dev@gmail.com>
2024-12-12 12:21:37 -0500

    feat: using expressMiddleware

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,35 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
 import mergedResolvers from "./resolvers/index.js"
 import mergedTypeDefs from "./typeDefs/index.js"
+
+const app = express();
+
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
+
+await server.start();
  
-const { url } = await startStandaloneServer(server)
- 
-console.log(`🚀 Server ready at ${url}`)
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
+
+await new Promise((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
