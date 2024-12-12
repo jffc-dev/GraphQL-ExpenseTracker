237de54f11351802170ea74330152e37df99484f
Javier Flores <jffc.dev@gmail.com>
2024-12-12 12:02:20 -0500

    feat: initial schemas created

---
+++ backend/typeDefs/index.js
@@ -0,0 +1,7 @@
+import {mergeTypeDefs} from "@graphql-tools/merge"
+import userTypeDef from "./user.typeDefs.js"
+import transactionTypeDef from "./transaction.typeDefs.js"
+
+const mergedTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef])
+
+export default mergedTypeDefs
